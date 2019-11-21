9c7d66f0ff9501b77e835613b341d98e05141ec9
Sharna Hossain <sharnajhossain@gmail.com>
2019-11-21 18:09:38 -0500

    figured out api

--- src/App.js
+++ src/App.js
@@ -3,20 +3,48 @@ import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
   state = {
-    books: []
+    books: [],
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
   }
+
   componentDidMount() {
-    BooksAPI.getAll
+    BooksAPI.getAll()
+      .then((books) => {
+        console.log(books)
+        this.setState(() => ({
+          books
+        }))
+      })
   }
+
   render() {
+    const { books } = this.state
     return (
       <div>
-        <CurrentlyReading />
-        <WantToRead />
-        <Read />
-        <SearchBooks />
+        <h1>MyReads</h1>
+          <h2>Currently Reading</h2>
+            <ul>
+              {books.filter((book) => book.shelf === "currentlyReading").map((book) => (
+                <li key={book.id}>{book.title}</li>
+              ))}
+            </ul>
+
+          <h2>Want to Read</h2>
+            <ul>
+              {books.filter((book) => book.shelf === "wantToRead").map((book) => (
+                <li key={book.id}>{book.title}</li>
+              ))}
+            </ul>
+          <h2>Read</h2>
+            <ul>
+              {books.filter((book) => book.shelf === "wantToRead").map((book) => (
+                <li key={book.id}>{book.title}</li>
+              ))}
+            </ul>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Shelf.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class Shelf extends Component {
+
+}
