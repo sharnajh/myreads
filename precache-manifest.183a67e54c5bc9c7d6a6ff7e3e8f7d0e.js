baf6e07950a09efa7f3861ea0169bde21009592f
Sharna Hossain <sharnajhossain@gmail.com>
2019-11-25 21:56:11 -0500

    Updates

---
+++ precache-manifest.183a67e54c5bc9c7d6a6ff7e3e8f7d0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e6bb18c2f67b56f1b30384b39bae13",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "6fb91fd854b2b20d9daa",
+    "url": "/myreads/static/css/main.b19dc129.chunk.css"
+  },
+  {
+    "revision": "be79f516193ea962da5f",
+    "url": "/myreads/static/js/2.7dae55d2.chunk.js"
+  },
+  {
+    "revision": "6fb91fd854b2b20d9daa",
+    "url": "/myreads/static/js/main.e0236717.chunk.js"
+  },
+  {
+    "revision": "f67be4ecdd23f5f3911b",
+    "url": "/myreads/static/js/runtime-main.1a9c4fd2.js"
+  },
+  {
+    "revision": "88a27840aec256d4a6af4257e1473942",
+    "url": "/myreads/static/media/drop-down-arrow.88a27840.svg"
+  },
+  {
+    "revision": "6c7df99d8ab3b8937a57be3d2dc16e1d",
+    "url": "/myreads/static/media/left.6c7df99d.svg"
+  },
+  {
+    "revision": "dee850f9c26b392fd38b85d0734e2658",
+    "url": "/myreads/static/media/no_cover_available.dee850f9.png"
+  }
+]);
